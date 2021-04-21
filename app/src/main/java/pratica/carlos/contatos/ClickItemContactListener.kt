5aebbe8dc79cd060ea78908f286c2c653e6a8e60
Carlos Manoel V A Costa <cavac10@gmail.com>
2021-04-21 13:18:27 -0300

    Initial commit

---
+++ app/src/main/java/pratica/carlos/contatos/ClickItemContactListener.kt
@@ -0,0 +1,5 @@
+package pratica.carlos.contatos
+
+interface ClickItemContactListener {
+    fun clickItemContact(contact: Contact)
+}
---
+++ app/src/main/java/pratica/carlos/contatos/MainActivity.kt
@@ -0,0 +1,115 @@
+package pratica.carlos.contatos
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.edit
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import pratica.carlos.contatos.DetailActivity.Companion.EXTRA_CONTACT
+
+class MainActivity : AppCompatActivity(), ClickItemContactListener {
+    private val rvList: RecyclerView by lazy {
+        findViewById(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.drawer_menu)
+
+        initDrawer()
+        fetchListContact()
+        bindViews()
+    }
+
+    private fun fetchListContact() {
+        val list = arrayListOf(
+            Contact(
+                    "Carlos",
+                    "(32)6466-9853",
+                    "img.png"
+            ),
+            Contact(
+                    "Filo",
+                    "(99)9999-9999",
+                    "img.png"
+            )
+        )
+        getInstanceSharedPreference().edit {
+            putString("contacts", Gson().toJson(list))
+        }
+    }
+
+    private fun getInstanceSharedPreference(): SharedPreferences {
+        return getSharedPreferences("pratica.carlos.contatos.PREFERENCE", Context.MODE_PRIVATE)
+    }
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        updateList()
+    }
+
+    private fun getListContacts(): List<Contact>{
+        val list = getInstanceSharedPreference().getString("contacts", "[]")
+        val turnsType = object : TypeToken<List<Contact>>() {}.type
+        return Gson().fromJson(list, turnsType)
+    }
+
+    private fun updateList() {
+        adapter.updateList(getListContacts())
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item do menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun clickItemContact(contact: Contact) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(EXTRA_CONTACT, contact)
+        startActivity(intent)
+    }
+}
